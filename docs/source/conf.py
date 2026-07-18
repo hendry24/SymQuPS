f33eb5b38ce6bd8082bb332568dfdf5bb2d42425
hendry24 <hendryadi01@gmail.com>
2026-07-18 20:11:23 +0800

    rtd

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,7 @@ html_static_path = ['_static']
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath("../../src"))
+sys.path.insert(0, os.path.abspath("../../src/symqups"))
 
 extensions = ["sphinx.ext.autodoc",
               "sphinx.ext.autosummary",
