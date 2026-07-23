e4bc8d22cf0de341f8bfd655f44945ac4ae94147
hendry24 <hendryadi01@gmail.com>
2026-07-23 19:52:47 +0700

    Updated benchmark. Removed derivative_not_in_sum. Updated docstring

--- src/symqups/utils.py
+++ src/symqups/utils.py
@@ -55,41 +55,11 @@ def get_random_poly(objects : Sequence,
                              for _ in range(random.randint(min_pow, max_pow))])
                     for _ in range(n_terms)])
     
-###
-
-@preprocess_func
-def derivative_not_in_num(A : sp.Expr) -> sp.Expr:
-    """
-    Rewrite the expression such that the phase-space coordinates and derivatives with respect
-    to them are not written on the numerator.
-    """
-    
-    if isinstance(A, sp.Add):
-        return sp.Add(*[derivative_not_in_num(arg) for arg in A.args], evaluate=False)
-    
-    if not A.has(sp.Derivative):
-        return A
-    
-    # `der_lst` here contains all the Derivative objects in a given term, 
-    # including the ones nested within another Derivative objects. We
-    # get the first one since it is the outermost.
-
-    args_without_der = list(A.args)
-    der_args = []
-    for arg in A.args:
-        if isinstance(arg, sp.Derivative):
-            args_without_der.remove(arg)
-            der_args.append(arg)
-    non_der = sp.Mul(*args_without_der)
-    der = sp.Mul(*der_args)
-    
-    with sp.evaluate(False):
-        return sp.Mul(non_der, der)
     
 @preprocess_func
 def collect_by_derivative(A : sp.Expr | sp.Equality) -> sp.Expr:
     """
-    Collect terms by the derivatives of the input function, by default those of the state function.
+    Collect terms containing the same ``sympy.Derivative`` object.
 
     Parameters
     ----------
